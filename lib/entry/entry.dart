b07602063ed29198d35adc41cd71353c1578c56d
WMXPY <wm@mengw.io>
2020-11-23 17:28:22 -0600

    fix import

--- lib/entry/entry.dart
+++ lib/entry/entry.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tanyao/entry/home.dart';
 
 class TanyaoEntry extends StatelessWidget {
