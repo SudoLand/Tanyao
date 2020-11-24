01db4d1eb06766ecdf156734515b89e2685ceccd
WMXPY <wm@mengw.io>
2020-11-24 16:13:02 -0600

    init gather list view

--- lib/routes/gather/gather.dart
+++ lib/routes/gather/gather.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:tanyao/i18n/core/localizations.dart';
 
 class GatherView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
+
     return Scaffold(
-      body: Center(
-        child: Text("Gather"),
+      body: ListView(
+        children: [
+          Card(
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.album),
+                    title: coreLocalizations.getText("riichi-mahjong"),
+                    subtitle: coreLocalizations.getText("riichi-mahjong"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
