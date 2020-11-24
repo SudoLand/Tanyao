e320dc0125a0e569d816ff724e9be6663268b140
WMXPY <wm@mengw.io>
2020-11-24 17:02:34 -0600

    implement game view

--- lib/routes/riichi/game.dart
+++ lib/routes/riichi/game.dart
@@ -12,9 +12,124 @@ class _RiichiGameViewState extends State<RiichiGameView> {
     final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
 
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          coreLocalizations.getText("Hello"),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Card(
+                child: coreLocalizations.getText("Hello"),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: RotatedBox(
+                quarterTurns: 1,
+                child: Card(
+                  child: coreLocalizations.getText("Hello"),
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: RotatedBox(
+                quarterTurns: 2,
+                child: Card(
+                  child: coreLocalizations.getText("Hello"),
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Card(
+                  child: coreLocalizations.getText("Hello"),
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: 150,
+                height: 150,
+                child: Card(
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Card(
+                            child: coreLocalizations.getText("Hello"),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: RotatedBox(
+                            quarterTurns: 1,
+                            child: Card(
+                              child: coreLocalizations.getText("Hello"),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: RotatedBox(
+                            quarterTurns: 2,
+                            child: Card(
+                              child: coreLocalizations.getText("Hello"),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: RotatedBox(
+                            quarterTurns: 3,
+                            child: Card(
+                              child: coreLocalizations.getText("Hello"),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: ClipOval(
+                            child: Material(
+                              child: InkWell(
+                                child: SizedBox(
+                                  width: 56,
+                                  height: 56,
+                                  child: Icon(Icons.menu),
+                                ),
+                                onTap: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
