4b0893f14a7faff28700b44a900be8077c0fb4bb
WMXPY <wm@mengw.io>
2020-11-24 19:19:12 -0600

    finish tile hand

--- lib/routes/common/tile/tile-hand.dart
+++ lib/routes/common/tile/tile-hand.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tanyao/entity/hand.dart';
 import 'package:tanyao/entity/mahjong.dart';
+import 'package:tanyao/routes/common/tile/tile-text.dart';
 
 class TileHandView extends StatelessWidget {
   final MahjongHand hand;
@@ -14,8 +15,12 @@ class TileHandView extends StatelessWidget {
     return Wrap(
       children: tiles
           .map(
-            (MahjongTile tile) => Text(
-              tile.toString(),
+            (MahjongTile tile) => Card(
+              margin: const EdgeInsets.all(1.0),
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                child: TileTextView(tile),
+              ),
             ),
           )
           .toList(),
