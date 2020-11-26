b89ce1806d2c40e573e16704b094e0f183b8811d
WMXPY <wm@mengw.io>
2020-11-26 14:42:54 -0600

    add recursiave trace mahjong

--- lib/mahjong/trace/trace.dart
+++ lib/mahjong/trace/trace.dart
@@ -1,11 +1,17 @@
 import 'package:tanyao/mahjong/hand.dart';
 import 'package:tanyao/mahjong/trace/result.dart';
 
+MahjongTraceResult _recursiveTraceMahjong(MahjongHand hand) {
+  final MahjongHand cloneHand = hand.clone();
+
+  return MahjongTraceResult();
+}
+
 MahjongTraceResult traceMahjong(MahjongHand hand) {
   final MahjongHand cloneHand = hand.clone();
 
   if (cloneHand.totalTiles() != 14) {
     return null;
   }
-  return MahjongTraceResult();
+  return _recursiveTraceMahjong(hand);
 }
