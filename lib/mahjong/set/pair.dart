eaba769be8b05c84a929999f6c14c327046221e3
WMXPY <wm@mengw.io>
2020-11-25 22:22:26 -0600

    init set types

---
+++ lib/mahjong/set/pair.dart
@@ -0,0 +1,3 @@
+import 'package:tanyao/mahjong/set/base.dart';
+
+class MahjongPairSet extends MahjongSet {}
---
+++ lib/mahjong/set/sequence.dart
@@ -0,0 +1,3 @@
+import 'package:tanyao/mahjong/set/base.dart';
+
+class MahjongSequenceSet extends MahjongSet {}
