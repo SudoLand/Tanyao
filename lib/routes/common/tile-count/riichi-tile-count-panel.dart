6cb24537f19ba2d188e4027ee63fac5a46d082cf
WMXPY <wm@mengw.io>
2020-11-25 21:33:45 -0600

    add tile count related panel

--- lib/routes/common/tile-count/riichi-tile-count-panel.dart
+++ lib/routes/common/tile-count/riichi-tile-count-panel.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tanyao/entity/mahjong.dart';
+import 'package:tanyao/i18n/core/localizations.dart';
+import 'package:tanyao/routes/common/tile/tile-input.dart';
 
 class RiichiTileCountPanelView extends StatefulWidget {
   @override
@@ -9,6 +12,21 @@ class RiichiTileCountPanelView extends StatefulWidget {
 class _RiichiTileCountPanelViewState extends State<RiichiTileCountPanelView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
+
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.face),
+          title: coreLocalizations.getText("result"),
+        ),
+        // ListTile(
+        //   title: TileHandView(this._hand),
+        // ),
+        TileInputView(
+          onPressed: (MahjongTile tile) {},
+        ),
+      ],
+    );
   }
 }
--- lib/routes/common/tile-count/show-tile-count-dialog.dart
+++ lib/routes/common/tile-count/show-tile-count-dialog.dart
@@ -1,33 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:tanyao/entity/mahjong.dart';
-import 'package:tanyao/i18n/core/localizations.dart';
 import 'package:tanyao/routes/common/side-dialog.dart';
-import 'package:tanyao/routes/common/tile/tile-input.dart';
+import 'package:tanyao/routes/common/tile-count/riichi-tile-count-panel.dart';
 
 void showTileCountDialog(
   BuildContext context, {
   @required int quarterTurns,
 }) {
-  final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
-
   showSideDialog(
     context,
     quarterTurns: quarterTurns,
-    width: 400,
+    width: 425,
     height: 250,
-    child: Column(
-      children: [
-        ListTile(
-          leading: Icon(Icons.face),
-          title: coreLocalizations.getText("result"),
-        ),
-        // ListTile(
-        //   title: TileHandView(this._hand),
-        // ),
-        TileInputView(
-          onPressed: (MahjongTile tile) {},
-        ),
-      ],
-    ),
+    child: RiichiTileCountPanelView(),
   );
 }
