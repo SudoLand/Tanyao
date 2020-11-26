e0f48535047b6896d5ff648946dd06cd4d801df9
WMXPY <wm@mengw.io>
2020-11-25 21:23:04 -0600

    init tile panel

---
+++ lib/routes/common/tile-count/riichi-tile-count-panel.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RiichiTileCountPanelView extends StatefulWidget {
+  @override
+  _RiichiTileCountPanelViewState createState() =>
+      _RiichiTileCountPanelViewState();
+}
+
+class _RiichiTileCountPanelViewState extends State<RiichiTileCountPanelView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
