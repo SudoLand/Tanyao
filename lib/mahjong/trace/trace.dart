db653e7683cb1658c73cf7599e33230a75c8689a
WMXPY <wm@mengw.io>
2020-11-28 14:48:24 -0600

    add combos logic

--- lib/mahjong/trace/trace.dart
+++ lib/mahjong/trace/trace.dart
@@ -1,17 +1,21 @@
 import 'package:tanyao/mahjong/hand.dart';
 import 'package:tanyao/mahjong/set/base.dart';
+import 'package:tanyao/mahjong/set/pair.dart';
 import 'package:tanyao/mahjong/set/triplet.dart';
 import 'package:tanyao/mahjong/tile.dart';
 import 'package:tanyao/mahjong/trace/result.dart';
 import 'package:tanyao/mahjong/type.dart';
 
-MahjongSet _getCombo(MahjongHand hand, MahjongTile target) {
+List<MahjongSet> _getCombos(MahjongHand hand, MahjongTile target) {
   final MahjongType type = target.type;
 
   // If current type is **wind** or **dragon**
   if (type == MahjongType.wind || type == MahjongType.dragon) {
     if (hand.getTileCount(target) == 3) {
-      return MahjongTripletSet(target);
+      return [MahjongTripletSet(target)];
+    }
+    if (hand.getTileCount(target) == 2) {
+      return [MahjongPairSet(target)];
     }
   }
 
@@ -19,20 +23,25 @@ MahjongSet _getCombo(MahjongHand hand, MahjongTile target) {
   if (type == MahjongType.bamboo ||
       type == MahjongType.character ||
       type == MahjongType.dot) {
+    final List<MahjongSet> possibleSets = [];
+
     if (hand.getTileCount(target) == 3) {
-      return MahjongTripletSet(target);
+      possibleSets.add(MahjongTripletSet(target));
     }
+
+    return possibleSets;
   }
 
-  return null;
+  return [];
 }
 
 MahjongTraceResult _recursiveTraceMahjong(MahjongHand hand) {
   final MahjongHand cloneHand = hand.clone();
 
   for (MahjongTile tile in MahjongTile.getAllAvailableTiles()) {
-    final MahjongSet combo = _getCombo(cloneHand, tile);
-    if (combo != null) {
+    final List<MahjongSet> combos = _getCombos(cloneHand, tile);
+
+    if (combos.isNotEmpty) {
       // TODO
     }
   }
