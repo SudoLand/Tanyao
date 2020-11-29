67baecb25fb3a1c2949b47ad5c3882fc366093b7
WMXPY <wm@mengw.io>
2020-11-29 16:33:29 -0600

    init finali result functions

---
+++ lib/mahjong/trace/finalize.dart
@@ -0,0 +1,15 @@
+import 'package:tanyao/mahjong/hand.dart';
+import 'package:tanyao/mahjong/trace/possible.dart';
+
+List<MahjongTracePossibleResult> finalizeMahjongPossibleResults(
+  MahjongHand hand,
+) {
+  final List<MahjongTracePossibleResult> finalizedResults = [];
+  final List<MahjongTracePossibleResult> possibleResults = [
+    MahjongTracePossibleResult(hand, []),
+  ];
+
+  while (possibleResults.isNotEmpty) {}
+
+  return [];
+}
--- lib/mahjong/trace/result.dart
+++ lib/mahjong/trace/result.dart
@@ -2,4 +2,5 @@ import 'package:tanyao/mahjong/trace/possible.dart';
 
 class MahjongTraceResult {
   final List<MahjongTracePossibleResult> possibleResults = [];
+  final List<MahjongTracePossibleResult> finalizedResults = [];
 }
