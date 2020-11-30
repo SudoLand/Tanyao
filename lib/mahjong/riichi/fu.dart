d06473e31f777ff2d054e1b9e306321651b9b9d9
WMXPY <wm@mengw.io>
2020-11-30 15:18:20 -0600

    add abse fu count

--- lib/mahjong/riichi/fu.dart
+++ lib/mahjong/riichi/fu.dart
@@ -1,13 +1,16 @@
 import 'package:tanyao/mahjong/set/base.dart';
 
+const int baseFu = 20;
+const int baseSevenPairsFu = 25;
+
 int calculateRiichiMahjongSevenPairsSetsFu(
   List<MahjongSet> mahjongSets,
 ) {
-  return 25;
+  return baseSevenPairsFu;
 }
 
 int calculateRiichiMahjongRegularSetsFu(
   List<MahjongSet> mahjongSets,
 ) {
-  return 0;
+  return baseFu;
 }
