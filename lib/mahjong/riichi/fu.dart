7d8c57f7c4651362589abaad33bd4de609c1bf19
WMXPY <wm@mengw.io>
2020-11-30 15:22:15 -0600

    add self pick and clear gate buffer

--- lib/mahjong/riichi/fu.dart
+++ lib/mahjong/riichi/fu.dart
@@ -1,4 +1,5 @@
 import 'package:tanyao/mahjong/set/base.dart';
+import 'package:tanyao/mahjong/set/pair.dart';
 
 const int baseFu = 20;
 const int baseSevenPairsFu = 25;
@@ -10,7 +11,22 @@ int calculateRiichiMahjongSevenPairsSetsFu(
 }
 
 int calculateRiichiMahjongRegularSetsFu(
-  List<MahjongSet> mahjongSets,
-) {
-  return baseFu;
+  List<MahjongSet> mahjongSets, {
+  bool clearGate = false,
+  bool selfPick = false,
+}) {
+  int resultFu = baseFu;
+
+  if (clearGate) {
+    resultFu += 10;
+  }
+  if (selfPick) {
+    resultFu += 2;
+  }
+
+  for (MahjongSet mahjongSet in mahjongSets) {
+    if (mahjongSet is MahjongPairSet) {}
+  }
+
+  return resultFu;
 }
