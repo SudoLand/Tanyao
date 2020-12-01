ac0c12122d1cb4cb13b91c90e94c805f0a98e36b
WMXPY <wm@mengw.io>
2020-11-30 18:23:16 -0600

    implement all green yaku implementation

--- lib/mahjong/riichi/yaku/all-green.dart
+++ lib/mahjong/riichi/yaku/all-green.dart
@@ -1,7 +1,21 @@
 import 'package:tanyao/mahjong/set/base.dart';
+import 'package:tanyao/mahjong/tile.dart';
 
 bool isAllGreen(
   List<MahjongSet> mahjongSets,
 ) {
-  return false;
+  for (MahjongSet mahjongSet in mahjongSets) {
+    for (MahjongTile tile in mahjongSet.tiles) {
+      if (!tile.isBamboo && tile != MahjongTile.fa()) {
+        return false;
+      }
+
+      if (tile.isBamboo) {
+        if (![2, 3, 4, 6, 8].contains(tile.value)) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
 }
