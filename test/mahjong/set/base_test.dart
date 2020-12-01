7f06cf34a30e08c6daf43e1fecd80031f988372c
WMXPY <wm@mengw.io>
2020-11-30 19:53:09 -0600

    add mocja set unit tests

--- test/mahjong/set/base_test.dart
+++ test/mahjong/set/base_test.dart
@@ -1,21 +1,49 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tanyao/mahjong/hand.dart';
 import 'package:tanyao/mahjong/set/base.dart';
 import 'package:tanyao/mahjong/tile.dart';
 
 class MahjongMockSet extends MahjongSet {
   MahjongMockSet(
-    MahjongTile tile,
-  ) : super(
-          [
-            tile,
-          ],
-        );
+    List<MahjongTile> tiles,
+  ) : super(tiles);
 }
 
 void main() {
-  test('can initialize hand with string - 1', () {
-    final MahjongHand hand = MahjongHand.fromString('ea');
-    expect(hand.toString(), equals('east'));
+  test('can be identify with is', () {
+    final MahjongMockSet mockSet = MahjongMockSet([]);
+
+    expect(mockSet is MahjongSet, equals(true));
+  });
+
+  test('can compare two mock set type - happy path', () {
+    final MahjongMockSet base = MahjongMockSet([
+      MahjongTile.dot(1),
+      MahjongTile.dot(2),
+      MahjongTile.dot(3),
+    ]);
+
+    final MahjongMockSet target = MahjongMockSet([
+      MahjongTile.dot(1),
+      MahjongTile.dot(2),
+      MahjongTile.dot(3),
+    ]);
+
+    expect(base, equals(target));
+  });
+
+  test('can compare two mock set type - sad path', () {
+    final MahjongMockSet base = MahjongMockSet([
+      MahjongTile.dot(1),
+      MahjongTile.dot(2),
+      MahjongTile.dot(3),
+    ]);
+
+    final MahjongMockSet target = MahjongMockSet([
+      MahjongTile.dot(3),
+      MahjongTile.dot(1),
+      MahjongTile.dot(2),
+    ]);
+
+    expect(base, isNot(equals(target)));
   });
 }
