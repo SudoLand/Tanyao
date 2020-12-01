884853f1ce0166d0b3fb5817880a48ec3b4bce20
WMXPY <wm@mengw.io>
2020-12-01 14:45:22 -0600

    move yaku dart, init fan calculation

---
+++ lib/mahjong/riichi/yaku/fan.dart
@@ -0,0 +1,9 @@
+import 'package:tanyao/mahjong/riichi/type.dart';
+
+int getRiichiFan(RiichiYakuType yakuType) {
+  return 0;
+}
+
+int calculateRiichiFan(List<RiichiYakuType> yakuTypes) {
+  return 0;
+}
