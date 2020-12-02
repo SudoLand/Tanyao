0ad916ba309252731075a7da1b3e91805f5a4f99
WMXPY <wm@mengw.io>
2020-12-01 18:09:28 -0600

    implement three colour straight yaku

--- lib/mahjong/riichi/yaku/regular/three-colour-straight.dart
+++ lib/mahjong/riichi/yaku/regular/three-colour-straight.dart
@@ -1,6 +1,6 @@
 import 'package:tanyao/mahjong/set/base.dart';
 import 'package:tanyao/mahjong/set/sequence.dart';
-import 'package:tanyao/mahjong/set/triplet.dart';
+import 'package:tanyao/mahjong/tile.dart';
 
 bool isRiichiThreeColourStraight(
   List<MahjongSet> mahjongSets,
@@ -11,9 +11,87 @@ bool isRiichiThreeColourStraight(
 
   for (MahjongSet mahjongSet in mahjongSets) {
     if (mahjongSet is MahjongSequenceSet) {
-      sequenceCount++;
+      if (mahjongSet.isDot) {
+        dotSets.add(mahjongSet);
+      }
+
+      if (mahjongSet.isBamboo) {
+        bambooSets.add(mahjongSet);
+      }
+
+      if (mahjongSet.isCharacter) {
+        characterSets.add(mahjongSet);
+      }
     }
   }
 
-  return sequenceCount >= 4;
+  if (dotSets.isEmpty || bambooSets.isEmpty || characterSets.isEmpty) {
+    return false;
+  }
+
+  MahjongSet initialSet;
+
+  if (dotSets.length == 1) {
+    initialSet = dotSets.first;
+  } else if (bambooSets.length == 1) {
+    initialSet = bambooSets.first;
+  } else if (characterSets.length == 1) {
+    initialSet = characterSets.first;
+  }
+
+  if (initialSet == null) {
+    return false;
+  }
+
+  bool dots = false;
+  bool bamboos = false;
+  bool characters = false;
+
+  final List<int> initialValues = [];
+  for (MahjongTile tile in initialSet.tiles) {
+    initialValues.add(tile.value);
+  }
+
+  initialValues.sort();
+
+  for (MahjongSequenceSet dotSet in dotSets) {
+    final List<int> values = [];
+    for (MahjongTile tile in dotSet.tiles) {
+      values.add(tile.value);
+    }
+
+    values.sort();
+
+    if (values.join(',') == initialValues.join(',')) {
+      dots = true;
+    }
+  }
+
+  for (MahjongSequenceSet bambooSet in bambooSets) {
+    final List<int> values = [];
+    for (MahjongTile tile in bambooSet.tiles) {
+      values.add(tile.value);
+    }
+
+    values.sort();
+
+    if (values.join(',') == initialValues.join(',')) {
+      bamboos = true;
+    }
+  }
+
+  for (MahjongSequenceSet characterSet in characterSets) {
+    final List<int> values = [];
+    for (MahjongTile tile in characterSet.tiles) {
+      values.add(tile.value);
+    }
+
+    values.sort();
+
+    if (values.join(',') == initialValues.join(',')) {
+      characters = true;
+    }
+  }
+
+  return dots && bamboos && characters;
 }
