b4ba8b74f2db83d8043511ba71725be86794b29a
WMXPY <wm@mengw.io>
2020-12-03 17:55:21 -0600

    init input hand view

---
+++ lib/routes/riichi/components/input/input-hand.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RiichiMahjongHandInputView extends StatefulWidget {
+  @override
+  _RiichiMahjongHandInputViewState createState() =>
+      _RiichiMahjongHandInputViewState();
+}
+
+class _RiichiMahjongHandInputViewState
+    extends State<RiichiMahjongHandInputView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
