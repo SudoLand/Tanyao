5549e9ae883d77135c39d277be1652d5fc70ce88
WMXPY <wm@mengw.io>
2020-12-03 18:01:01 -0600

    link input hand view

--- lib/components/riichi/input-hand.dart
+++ lib/components/riichi/input-hand.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tanyao/components/tile/tile-hand.dart';
+import 'package:tanyao/components/tile/tile-input.dart';
+import 'package:tanyao/i18n/core/localizations.dart';
+import 'package:tanyao/mahjong/hand.dart';
+import 'package:tanyao/mahjong/tile.dart';
 
 class RiichiMahjongHandInputView extends StatefulWidget {
   @override
@@ -8,8 +13,37 @@ class RiichiMahjongHandInputView extends StatefulWidget {
 
 class _RiichiMahjongHandInputViewState
     extends State<RiichiMahjongHandInputView> {
+  final MahjongHand _hand = MahjongHand();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
+
+    return Container(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.face),
+            title: coreLocalizations.getText("result"),
+          ),
+          Container(
+            margin: const EdgeInsets.all(5.0),
+            height: 70,
+            child: TileHandView(
+              this._hand,
+              onRemovePressed: (MahjongTile tile) {
+                this._hand.removeTile(tile);
+                this.setState(() {});
+              },
+            ),
+          ),
+          TileInputView(
+            onPressed: (MahjongTile tile) {
+              this._hand.addTile(tile);
+              this.setState(() {});
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/routes/tools/point-calculator/riichi-point-calculator.dart
+++ lib/routes/tools/point-calculator/riichi-point-calculator.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tanyao/components/tile/tile-hand.dart';
-import 'package:tanyao/components/tile/tile-input.dart';
+import 'package:tanyao/components/riichi/input-hand.dart';
 import 'package:tanyao/i18n/core/localizations.dart';
-import 'package:tanyao/mahjong/hand.dart';
-import 'package:tanyao/mahjong/tile.dart';
 
 class RiichiPointCalculatorToolView extends StatefulWidget {
   @override
@@ -13,8 +10,6 @@ class RiichiPointCalculatorToolView extends StatefulWidget {
 
 class _RiichiPointCalculatorToolViewState
     extends State<RiichiPointCalculatorToolView> {
-  final MahjongHand _hand = MahjongHand();
-
   @override
   Widget build(BuildContext context) {
     final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
@@ -28,34 +23,12 @@ class _RiichiPointCalculatorToolViewState
       ),
       body: Column(
         children: [
-          Card(
-            margin: const EdgeInsets.only(
-              top: 10.0,
-              left: 10.0,
-              right: 10.0,
-            ),
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(Icons.face),
-                  title: coreLocalizations.getText("result"),
-                ),
-                ListTile(
-                  title: TileHandView(this._hand),
-                ),
-              ],
-            ),
-          ),
+          Card(),
           Card(
             margin: const EdgeInsets.all(10.0),
             child: Container(
               padding: const EdgeInsets.all(15.0),
-              child: TileInputView(
-                onPressed: (MahjongTile tile) {
-                  this._hand.addTile(tile);
-                  this.setState(() {});
-                },
-              ),
+              child: RiichiMahjongHandInputView(),
             ),
           ),
         ],
