5f41fc0aace50b54e68ee1ea3bbdb4e69e75cfb1
WMXPY <wm@mengw.io>
2020-12-05 15:45:30 -0600

    add next step navigation

--- lib/components/riichi/input-hand-navigator/tile-input.dart
+++ lib/components/riichi/input-hand-navigator/tile-input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tanyao/components/tile/tile-hand.dart';
 import 'package:tanyao/components/tile/tile-input.dart';
+import 'package:tanyao/i18n/core/localizations.dart';
 import 'package:tanyao/mahjong/hand.dart';
 import 'package:tanyao/mahjong/tile.dart';
 
@@ -15,9 +16,13 @@ class RiichiMahjongHandInputNavigatorTileInputView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final CoreLocalizations coreLocalizations = CoreLocalizations.of(context);
+
     final MahjongHand cloneHand = this.hand.clone();
 
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
         TileHandView(
           cloneHand,
@@ -41,7 +46,9 @@ class RiichiMahjongHandInputNavigatorTileInputView extends StatelessWidget {
           height: 10.0,
         ),
         RaisedButton(
-          child: Text("123"),
+          child: coreLocalizations.getText("next-step!1", replacements: {
+            "route": coreLocalizations.getString("select-result"),
+          }),
           onPressed: () {
             Navigator.of(context).pushNamed(
               'riichi-input-hand-navigator/#/select-result',
